ce8850d0410fb3e07f159fda3faed1e66dcfd0fc
PetrovAndrii <andrey9314@gmail.com>
2018-08-08 12:50:42 +0300

    покупка билета
    В будущем переделать:
     - выбирать только активный поезд
     - выбирать только активное местро
     - придумать обход капчи (возможно учесть все моменты когда она может быть и просто вставлять туда придуманный в будущем код обхода)

--- test_buy_ticket.py
+++ test_buy_ticket.py
@@ -4,8 +4,6 @@ import pytest
 from application import Application
 
 
-
-
 @pytest.fixture
 def app(request):
     fixture = Application()
@@ -13,17 +11,12 @@ def app(request):
     return fixture
 
 
-
-def test_authorization(app):
+def test_buy_ticket(app):
     app.open_mobile_page()
     app.login(username="uz.all.test@gmail.com", password="P@ssw0rd")
-    app.logout()
-
-
-def test_authorization_google(app):
-    app.open_mobile_page()
-    app.login_google(username="uz.all.test@gmail.com", password="P@ssw0rd!@#")
-    app.logout()
-
-
-
+    app.search_train(from_station="Київ", to_station="Одеса")
+    app.choice_train()
+    app.choice_types()
+    app.choice_plase()
+    app.doc_type_full(last_name="test", first_name="uz")
+    app.pay(email_pay="uz.all.test@gmail.com")
